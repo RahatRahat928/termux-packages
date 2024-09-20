8038efe5f2fc99d945b73c09aac042e58a66c0b3
Fredrik Fornwall <fredrik@fornwall.net>
2024-09-20 11:41:40 +0200

    revbump(main/ccache): Rebuild after fmt update

--- packages/ccache/build.sh
+++ packages/ccache/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Compiler cache for fast recompilation of C/C++ code"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="4.10.2"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/ccache/ccache/releases/download/v$TERMUX_PKG_VERSION/ccache-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_SHA256=c0b85ddfc1a3e77b105ec9ada2d24aad617fa0b447c6a94d55890972810f0f5a
 TERMUX_PKG_AUTO_UPDATE=true
